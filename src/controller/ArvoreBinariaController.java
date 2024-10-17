59651f533a8d1d979b5a4d855a2daf5175dd3566
Lucas <lucaswar05@gmail.com>
2024-10-16 21:54:03 -0300

    New Repository

---
+++ src/controller/ArvoreBinariaController.java
@@ -0,0 +1,7 @@
+package controller;
+
+public class ArvoreBinariaController {
+    public ArvoreBinariaController(){
+        super();
+    }
+}
---
+++ src/view/ArvoreBinaria.java
@@ -0,0 +1,7 @@
+package view;
+
+public class ArvoreBinaria {
+    public static void main(String[] args) {
+        
+    }
+}
