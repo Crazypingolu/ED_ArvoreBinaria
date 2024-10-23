c7d09049a1bc5b6994ba9c38b89d814d6899414f
Lucas P. Oliveira <lucaswar05@gmail.com>
2024-10-22 22:13:42 -0300

    Update  No.java

--- src/model/estrutura/lista/No.java
+++ src/model/estrutura/lista/No.java
@@ -8,28 +8,22 @@ public class No<T>{
 		this.proximo = null;
 		this.valor = valor;
 	}
-	
 	public No(T valor, No<T> proximo) {
 		this.valor = valor;
 		this.proximo = proximo;
 	}
-	
 	public T getValor(){
 		return valor;
 	}
-	
 	public void setValor(T valor){
 		this.valor = valor;
 	}
-	
 	public No<T> getProximo(){
 		return proximo;
 	}
-	
 	public void setProximo(No<T> proximo){
 		this.proximo = proximo;
 	}
-	
 	@Override
 	public String toString(){
 		return valor.toString();
